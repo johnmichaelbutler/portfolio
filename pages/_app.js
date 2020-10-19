74892ab8dec491c25426eee7d858f59f44d42b08
John Butler <johnmichaelbutler@yahoo.com>
2020-10-18 21:01:06 -0500

    Update background

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <meta charset="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>John Butler</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
 
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,75 +1,72 @@
 module.exports = {
-  // purge: [
-  //   './src/**/*.html',
-  //   './src/**/*.js',
-  // ],
+  purge: ['./src/**/*.html', './src/**/*.js'],
   theme: {
     maxWidth: {
-      '3/4': '75%'
+      '3/4': '75%',
     },
     extend: {
       fontFamily: {
-        'lobster': ['Lobster'],
-        'oswald': ['Oswald']
+        lobster: ['Lobster'],
+        oswald: ['Oswald'],
       },
       width: {
-        '9': '90',
-        '257': '257px',
-        '317': '317px',
+        9: '90',
+        257: '257px',
+        317: '317px',
         '90vw': '90vw',
         '50vw': '50vw',
         '70vw': '70vw',
-        '500': '500px',
-        '390': '390px',
-        '220': '220px'
+        500: '500px',
+        390: '390px',
+        220: '220px',
       },
       colors: {
-        'primary': '#2d3748',
+        primary: '#2d3748',
         'bg-light': '#f2f2f2',
         'bg-dark': '#696969',
         'burnt-orange': 'rgba(183, 109, 12, 60%)',
-        'linkedin-blue':'#4875B4',
+        'linkedin-blue': '#4875B4',
         'twitter-blue': '#00ACED',
-        'github-purple': '#320782'
+        'github-purple': '#320782',
       },
       height: {
         '1/2': '50vh',
         '76vh': '76vh',
-        '700': '700px',
-        '641': '641px',
+        700: '700px',
+        641: '641px',
       },
       opacity: {
-        '90': '90%'
+        90: '90%',
       },
       inset: {
         '-1/4': '-25vh',
         '-94': '-94%',
         '8%': '8%',
-        '100': '100%',
-        '10': '10px',
+        100: '100%',
+        10: '10px',
       },
       screens: {
-        '2xl': '1500px'
+        '2xl': '1500px',
       },
       keyframes: {
         'fade-in-left': {
-          '0%': { transform: 'translate(-100%)', opacity: '0'},
-          '100%': { transform: 'translate(0)', opacity: '1'}
+          '0%': { transform: 'translate(-100%)', opacity: '0' },
+          '100%': { transform: 'translate(0)', opacity: '1' },
         },
         'fade-in-right': {
-          '0%': { transform: 'translate(100%)', opacity: '0'},
-          '100%': { transform: 'translate(0)', opacity: '1'}
+          '0%': { transform: 'translate(100%)', opacity: '0' },
+          '100%': { transform: 'translate(0)', opacity: '1' },
         },
         'fade-in': {
-          '0%': { opacity: '0'},
-          '100%': { opacity: '1'}
-        }
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
       },
       animation: {
         'fade-in-left': 'fade-in-left .7s ease-in-out',
         'fade-in-right': 'fade-in-right .7s ease-in-out',
         'fade-in': 'fade-in 1s ease-in',
-      }
+      },
     },
   },
   future: {
@@ -84,10 +81,8 @@ module.exports = {
     translate: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
     boxShadow: ['responsive', 'hover', 'focus', 'active'],
   },
-  plugins: [
-    require('tailwindcss-scroll-snap')
-  ],
-}
+  plugins: [require('tailwindcss-scroll-snap')],
+};
 
 // 'color-transition': {
 //   '0%': { color: '#2d3748', backgroundColor: 'white'},
